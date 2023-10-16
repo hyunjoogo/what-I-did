15decd619c5d7262a85e19bb9ccc3219e53886b0
HyunJoo Kim <hyunjoogo@gmail.com>
2023-10-16 15:49:00 +0900

    #27 없는 행동아이디를 입력시 생성 페이지로 이동할 수 있도록 구현

--- src/contexts/ActionProgressProvider.tsx
+++ src/contexts/ActionProgressProvider.tsx
@@ -2,15 +2,24 @@ import { createContext, PropsWithChildren, useContext } from 'react';
 import { CurrentActionInfo } from '../types/action';
 import useLocalStorage from '../hooks/localStorage/useLocalStorage';
 import useFetch from '../hooks/useFetch';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ROUTES_PATH } from '../constants/routes';
 
 const CurrentActionInfoContext = createContext<CurrentActionInfo | null>(null);
 
 const ActionProgressProvider = ({ children }: PropsWithChildren) => {
+  const { actionId } = useParams();
+  const navigate = useNavigate();
+
+  if (!actionId) throw new Error('정상적인 경로로 접근해주세요.');
+
   const { getCurrentAction } = useLocalStorage();
   const { result: currentActionInfo } = useFetch(() => getCurrentAction());
 
   if (!currentActionInfo) return null;
-
+  if (Number(actionId) !== currentActionInfo.startTimestamp) {
+    navigate(`${ROUTES_PATH.create}`);
+  }
   return <CurrentActionInfoContext.Provider value={currentActionInfo}>{children}</CurrentActionInfoContext.Provider>;
 };
 
--- src/pages/ActionProgress.tsx
+++ src/pages/ActionProgress.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import styled from 'styled-components';
 import InActionBoard from '../components/progress/InActionBoard/InActionBoard';
 import ActionProgressProvider from '../contexts/ActionProgressProvider';
+import LoadingFallback from '../components/common/LoadingFallback/LoadingFallback';
 
 const ActionProgress = () => {
   return (
     <Layout>
-      <ActionProgressProvider>
-        <InActionBoard></InActionBoard>
-      </ActionProgressProvider>
+      <Suspense fallback={<LoadingFallback height="100vh" />}>
+        <ActionProgressProvider>
+          <InActionBoard></InActionBoard>
+        </ActionProgressProvider>
+      </Suspense>
     </Layout>
   );
 };
