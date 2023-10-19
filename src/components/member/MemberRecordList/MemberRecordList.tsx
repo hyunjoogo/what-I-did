5e8666b6982cebae047e3941f8128a63c95ee76f
HyunJoo Kim <hyunjoogo@gmail.com>
2023-10-19 16:01:22 +0900

    #35 클릭시 record/actionid로 이동

--- src/components/member/MemberRecordList/MemberRecordList.tsx
+++ src/components/member/MemberRecordList/MemberRecordList.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import { useActionPlansInfo } from '../../../contexts/ActionPlansProvider';
 import MemberRecordItem from '../MemberRecordItem/MemberRecordItem';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES_PATH } from '../../../constants/routes';
 
 const MemberRecordList = () => {
   const { plans } = useActionPlansInfo();
-
+  const navigate = useNavigate();
   const handleClickItem = (actionId: number) => {
-    console.log(actionId);
+    navigate(`${ROUTES_PATH.record}/${actionId}`);
   };
 
   return (
     <Layout>
-      {plans.map((action, index) => (
+      {plans.reverse().map((action, index) => (
         <MemberRecordItem key={index} action={action} handleClickItem={handleClickItem} />
       ))}
     </Layout>
